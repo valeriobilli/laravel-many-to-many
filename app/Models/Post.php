ce306dfb473acf81bf048e8cf3b2c817b91f2b74
Valerio Billi <valerio.billi18@gmail.com>
2023-07-17 14:45:37 +0200

    first commit

---
+++ app/Models/Post.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "title",
+        "content",
+        "image",
+        "type_id",
+    ];
+
+    public function type() {
+        return $this->belongsTo(Type::class);
+    }
+}
+
+
