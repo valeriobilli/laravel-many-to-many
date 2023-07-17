b42610f15f34af08ea2af1fd9c9fab7cfe6498c8
Valerio Billi <valerio.billi18@gmail.com>
2023-07-17 15:06:51 +0200

    create model technolies, make migration e relation many to many

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -19,6 +19,11 @@ class Post extends Model
     public function type() {
         return $this->belongsTo(Type::class);
     }
+
+    public function technologies() {
+        return $this->belongToMany('App\Models\Technology');
+    }
+
 }
 
 
