b944631c7bc247838352974711cf3a59abb7b044
Valerio Billi <valerio.billi18@gmail.com>
2023-07-17 15:10:45 +0200

    make technology seeder

---
+++ database/seeders/TechnologySeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class TechnologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i=0; $i < 20 ; $i++) { 
+            $post = new Technology();
+            $post->name = $faker->word(1);
+            $post->save();
+        }
+    }
+}
+
